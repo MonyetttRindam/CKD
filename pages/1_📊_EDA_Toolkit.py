4a56611ffe6b3b9d4adf9b29605c32ac73f1076d
MonyetttRindam <khoiriabil13@gmail.com>
2026-06-02 10:35:16 +0700

    Update 1_📊_EDA_Toolkit.py

--- pages/1_📊_EDA_Toolkit.py
+++ pages/1_📊_EDA_Toolkit.py
@@ -9,7 +9,7 @@ st.markdown("Dataset **mentah (sebelum preprocessing)**. Gunakan panel kiri untu
 # Load dataset
 @st.cache_data
 def load_data():
-    df = pd.read_csv("ckd_dataset.csv")
+    df = pd.read_csv("CKD_NHANES_2021_2023")
     return df
 
 try:
